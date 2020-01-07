b8d3b387b2b775fd62727e92c7e68c2989763a27
Sada <sadanandegowda.dm@mindtree.com>
2020-01-07 12:42:27 +0530

    Updated deployment scripts as part of repo split
    Former-commit-id: ec18491b82f43fa174963c420fd442cf8a5f36e6

--- db_release_scripts/mosip_prereg/sql/1.0.5_prereg-scripts_revoke.sql
+++ db_release_scripts/mosip_prereg/sql/1.0.5_prereg-scripts_revoke.sql
@@ -1,9 +1,9 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_prereg
--- Release Version 	: 1.0.4
+-- Release Version 	: 1.0.5
 -- Purpose    		: Revoking Database Alter deployement done for release in Pre registration DB.       
 -- Create By   		: Sadanandegowda DM
--- Created Date		: 30-Dec-2019
+-- Created Date		: 03-Jan-2020
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
