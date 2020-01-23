fd83a03e09aaf2d919de46fde3b3c1f0ad6d1fcb
Kishan Rathore <Kishan.Rathore2@mindtree.com>
2020-01-23 17:00:41 +0530

    Revert "MOS-30943_Making non static fields as static keyword is not required"
    This reverts commit 041e2ed096fe39829bf2153b91d78109b2ff5d6a.

--- pre-registration/pre-registration-login-service/src/main/java/io/mosip/preregistration/login/service/LoginService.java
+++ pre-registration/pre-registration-login-service/src/main/java/io/mosip/preregistration/login/service/LoginService.java
@@ -121,8 +121,8 @@ public class LoginService {
 	@Qualifier("restTemplateConfig")
 	private RestTemplate restTemplate;
 
-	private String globalConfig;
-	private String preregConfig;
+	private static String globalConfig;
+	private static String preregConfig;
 
 	@PostConstruct
 	public void setupLoginService() {
