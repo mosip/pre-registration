c6de66d6338e79ff007b86ce78de93bed3207d97
Ajay1596 <ajay.j2@mindtree.com>
2021-01-04 14:40:08 +0530

    adding endpoint to update docRefId

--- pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/service/DocumentServiceIntf.java
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/service/DocumentServiceIntf.java
@@ -3,11 +3,11 @@ package io.mosip.preregistration.application.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.mosip.preregistration.application.dto.DocumentResponseDTO;
 import io.mosip.preregistration.core.common.dto.DocumentDTO;
 import io.mosip.preregistration.core.common.dto.DocumentDeleteResponseDTO;
 import io.mosip.preregistration.core.common.dto.DocumentsMetaData;
 import io.mosip.preregistration.core.common.dto.MainResponseDTO;
-import io.mosip.preregistration.application.dto.DocumentResponseDTO;
 
 @Service
 public interface DocumentServiceIntf {
@@ -16,10 +16,8 @@ public interface DocumentServiceIntf {
 	 * This method is used to upload the document by accepting the JsonString and
 	 * MultipartFile
 	 * 
-	 * @param file
-	 *            pass the file
-	 * @param documentJsonString
-	 *            pass document json
+	 * @param file               pass the file
+	 * @param documentJsonString pass document json
 	 * @return ResponseDTO
 	 */
 	public MainResponseDTO<DocumentResponseDTO> uploadDocument(MultipartFile file, String documentJsonString,
@@ -29,12 +27,9 @@ public interface DocumentServiceIntf {
 	 * This method is used to copy the document from source preId to destination
 	 * preId
 	 * 
-	 * @param catCode
-	 *            pass category code
-	 * @param sourcePreId
-	 *            pass source preRegistrationId
-	 * @param destinationPreId
-	 *            pass destination preRegistrationId
+	 * @param catCode          pass category code
+	 * @param sourcePreId      pass source preRegistrationId
+	 * @param destinationPreId pass destination preRegistrationId
 	 * @return ResponseDTO
 	 */
 	MainResponseDTO<DocumentResponseDTO> copyDocument(String catCode, String sourcePreId, String destinationPreId);
@@ -42,8 +37,7 @@ public interface DocumentServiceIntf {
 	/**
 	 * This method is used to get all the documents for a preId
 	 * 
-	 * @param preId
-	 *            pass preRegistrationId
+	 * @param preId pass preRegistrationId
 	 * @return ResponseDTO
 	 */
 	MainResponseDTO<DocumentsMetaData> getAllDocumentForPreId(String preId);
@@ -51,10 +45,8 @@ public interface DocumentServiceIntf {
 	/**
 	 * This method is used to get particular document for a docId
 	 * 
-	 * @param docId
-	 *            pass documentId
-	 * @param preId
-	 *            pass preRegistrationId
+	 * @param docId pass documentId
+	 * @param preId pass preRegistrationId
 	 * @return ResponseDTO
 	 */
 	MainResponseDTO<DocumentDTO> getDocumentForDocId(String docId, String preId);
@@ -62,8 +54,7 @@ public interface DocumentServiceIntf {
 	/**
 	 * This method is used to delete the document for document Id
 	 * 
-	 * @param documentId
-	 *            pass documentID
+	 * @param documentId pass documentID
 	 * @return ResponseDTO
 	 */
 	MainResponseDTO<DocumentDeleteResponseDTO> deleteDocument(String documentId, String preRegistrationId);
@@ -71,10 +62,21 @@ public interface DocumentServiceIntf {
 	/**
 	 * This method is used to delete all the documents for a preId
 	 * 
-	 * @param preregId
-	 *            pass preRegistrationId
+	 * @param preregId pass preRegistrationId
 	 * @return ResponseDTO
 	 */
 	MainResponseDTO<DocumentDeleteResponseDTO> deleteAllByPreId(String preregId);
 
+	/**
+	 * This method is used to update the docRefId
+	 * 
+	 * @param documentId
+	 * 
+	 * @param preregId
+	 * 
+	 * @param docRefId
+	 * 
+	 * @return ResponseDTO
+	 */
+	MainResponseDTO<String> updateDocRefId(String documentId, String preId, String docRefId);
 }
