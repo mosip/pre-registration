2409234e01fbc4464747acebfb6bce57a944a5a0
Ajay1596 <44763643+Ajay1596@users.noreply.github.com>
2021-01-25 12:19:56 +0530

    Update RestInterceptor.java

--- pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/config/RestInterceptor.java
+++ pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/config/RestInterceptor.java
@@ -38,7 +38,7 @@ public class RestInterceptor implements ClientHttpRequestInterceptor {
 			ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
 		LOGGER.info("Request url: " + httpRequest.getURI());
 		if (!httpRequest.getURI().toString().contains("authmanager")) {
-			if (httpRequest.getURI().toString().contains("preregistration") && !httpRequest.getURI().toString().contains("sync")) {
+			if (httpRequest.getURI().toString().contains("preregistration")) {
 				HttpHeaders headers = httpRequest.getHeaders();
 				LOGGER.info("Reterving prereg token: ");
 				String token = getAuthUserDetails().getToken();
