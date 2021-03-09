1e866e34b5748fa90169db5983f8f348c6db4fa6
ckm007 <chandrakeshavmishra@gmail.com>
2021-03-09 07:31:12 +0530

    updated for release

--- db_release_scripts/mosip_prereg/sql/1.1.4_prereg-scripts_release.sql
+++ db_release_scripts/mosip_prereg/sql/1.1.4_prereg-scripts_release.sql
@@ -1,6 +1,6 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_prereg
--- Release Version 	: 1.1.4-rc1
+-- Release Version 	: 1.1.4
 -- Purpose    		: Database Alter scripts for the release for Pre Registration DB.       
 -- Create By   		: Sadanandegowda DM
 -- Created Date		: Dec-2020
