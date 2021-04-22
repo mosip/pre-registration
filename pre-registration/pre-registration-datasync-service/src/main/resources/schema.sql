d7b947321c6d8d669f72661c44fa964fb3fd5d16
Ajay1596 <ajay.j2@mindtree.com>
2021-04-22 10:38:31 +0530

    sql changes

--- pre-registration/pre-registration-datasync-service/src/main/resources/schema.sql
+++ pre-registration/pre-registration-datasync-service/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE prereg.machine_encryption_key(
+CREATE TABLE IF NOT EXISTS prereg.machine_encryption_key(
 	machine_id character varying(36),
 	encrypted_publickey character varying,
 );
