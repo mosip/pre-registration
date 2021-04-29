f871ad165092a5795449e1f85665139cd3456cdb
Ajay1596 <ajay.j2@mindtree.com>
2021-04-29 18:28:33 +0530

    response changes

---
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/dto/UISpecMetaDataDTO.java
@@ -0,0 +1,23 @@
+package io.mosip.preregistration.application.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class UISpecMetaDataDTO {
+
+	private String id;
+	private double version;
+	private String title;
+	private String description;
+	private String identitySchemaId;
+	private double idSchemaVersion;
+	private String jsonSpec;
+	private String status;
+	private LocalDateTime effectiveFrom;
+	private LocalDateTime createdOn;
+	private LocalDateTime updatedOn;
+
+}
