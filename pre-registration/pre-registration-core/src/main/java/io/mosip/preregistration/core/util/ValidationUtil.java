fa0eeef113c55a62a9c0cc6e834463947d9acef9
Ajay1596 <44763643+Ajay1596@users.noreply.github.com>
2021-05-04 19:03:27 +0530

    MOSIP-12409 adding cancelled check (#209)
    * MOSIP-10951
    
    * MOSIP-10951
    
    * MOSIP-10951
    
    * resolving confilct
    
    * resolved confict
    
    * MOSIP-12409 adding cancelled check

--- pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/util/ValidationUtil.java
+++ pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/util/ValidationUtil.java
@@ -386,6 +386,8 @@ public class ValidationUtil {
 			isStatusBookedOrExpired = !isStatusBookedOrExpired;
 		} else if (StatusCodes.EXPIRED.getCode().equals(status)) {
 			isStatusBookedOrExpired = !isStatusBookedOrExpired;
+		} else if (StatusCodes.CANCELED.getCode().equals(status)) {
+			isStatusBookedOrExpired = !isStatusBookedOrExpired;
 		}
 		return isStatusBookedOrExpired;
 	}
