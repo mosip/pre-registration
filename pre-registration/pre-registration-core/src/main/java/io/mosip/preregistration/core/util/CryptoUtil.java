a6086835042937b378d57ef1c8ea044657ef5d54
Ajay1596 <ajay.j2@mindtree.com>
2021-05-17 15:13:11 +0530

    removing temp file

--- pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/util/CryptoUtil.java
+++ pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/util/CryptoUtil.java
@@ -98,8 +98,7 @@ public class CryptoUtil {
 		ResponseEntity<ResponseWrapper<CryptoManagerResponseDTO>> response = null;
 		byte[] decodedBytes = null;
 		try {
-            System.out.println(new String(originalInput, StandardCharsets.UTF_8));
-            System.out.println(localDateTime);
+  
 			CryptoManagerRequestDTO dto = new CryptoManagerRequestDTO();
 			dto.setApplicationId(cryptoApplcationId);
 			dto.setData(new String(originalInput, StandardCharsets.UTF_8));
