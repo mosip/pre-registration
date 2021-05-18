392093e502960948f144b637ad8d18b24bb9c224
Ajay1596 <ajay.j2@mindtree.com>
2021-05-18 15:51:24 +0530

    adding configuration for cache

--- pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/config/CacheConfig.java
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/config/CacheConfig.java
@@ -5,7 +5,6 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -26,9 +25,14 @@ public class CacheConfig extends CachingConfigurerSupport {
 		masterDataCache.setMemoryStoreEvictionPolicy(
 				env.getProperty("preregistration.masterdata.cache.memoryEvictionPolicy", "LRU"));
 		masterDataCache.setMaxEntriesLocalHeap(
-				Integer.parseInt(env.getProperty("preregistration.masterdata.cache.maxEntriesLocalHeap", "2000")));
+				Integer.parseInt(env.getProperty("preregistration.masterdata.cache.maxEntriesLocalHeap", "10000")));
 		masterDataCache.setTimeToLiveSeconds(
 				Integer.parseInt(env.getProperty("preregistration.masterdata.cache.TimeToLiveSeconds", "3600")));
+		masterDataCache
+				.setLogging(Boolean.parseBoolean(env.getProperty("preregistration.masterdata.cache.setLogging")));
+		masterDataCache
+				.setEternal(Boolean.parseBoolean(env.getProperty("preregistration.masterdata.cache.setEternal")));
+		masterDataCache.setMaxBytesLocalDisk(env.getProperty("preregistration.masterdata.cache.setMaxBytesLocalDisk"));
 
 		net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
 		config.addCache(masterDataCache);
@@ -40,4 +44,5 @@ public class CacheConfig extends CachingConfigurerSupport {
 	public CacheManager cacheManager() {
 		return new EhCacheCacheManager(ehCacheManager());
 	}
+
 }
