c0e9bfe6b408e7d9b7287141e815adbdf8198b79
Ajay1596 <ajay.j2@mindtree.com>
2021-06-02 10:30:49 +0530

    implemented code review comments changes

--- pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/controller/LoginController.java
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/controller/LoginController.java
@@ -68,6 +68,9 @@ public class LoginController {
 
 	@Autowired
 	private Environment environment;
+	
+	@Value("${mosip.kernel.otp.expiry-time}")
+	private int optExpiryTime;
 
 	@Value("${mosip.kernel.otp.expiry-time}")
 	private int otpExpiryTime;
@@ -237,7 +240,6 @@ public class LoginController {
 		if (Objects.isNull(response.getErrors())) {
 			Cookie resCookie = new Cookie("canAuthorise",
 					loginService.sendOTPSuccessJwtToken(sendOtpRequestWithCaptcha.getRequest().getUserId()));
-
 			resCookie.setMaxAge((int)  otpExpiryTime/ 60);
 			resCookie.setHttpOnly(true);
 			resCookie.setSecure(true);
