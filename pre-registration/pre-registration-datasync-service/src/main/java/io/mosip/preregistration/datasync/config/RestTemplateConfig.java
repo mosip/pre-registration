91e1b4749031205ed24d52c5ea82549d78f3235c
Ajay1596 <44763643+Ajay1596@users.noreply.github.com>
2021-07-07 12:41:27 +0530

    Update RestTemplateConfig.java

--- pre-registration/pre-registration-datasync-service/src/main/java/io/mosip/preregistration/datasync/config/RestTemplateConfig.java
+++ pre-registration/pre-registration-datasync-service/src/main/java/io/mosip/preregistration/datasync/config/RestTemplateConfig.java
@@ -17,8 +17,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import io.mosip.kernel.auth.defaultadapter.config.RequesterTokenRestInterceptor;
-
 @Configuration
 public class RestTemplateConfig {
 
@@ -40,7 +38,7 @@ public class RestTemplateConfig {
 
 		RestTemplate restTemplate = new RestTemplate(requestFactory);
 
-		restTemplate.setInterceptors(Collections.singletonList(new RequesterTokenRestInterceptor()));
+		restTemplate.setInterceptors(Collections.singletonList(new RestInterceptor()));
 
 		return restTemplate;
 
