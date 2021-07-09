e1aca64ea7c738cd400c267e979462edc46b805d
Ajay1596 <ajay.j2@mindtree.com>
2021-07-09 21:55:03 +0530

    MOSIP-14587

--- pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/config/DataSyncRestInterceptor.java
+++ pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/config/DataSyncRestInterceptor.java
@@ -9,13 +9,15 @@ import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import io.mosip.kernel.core.authmanager.authadapter.model.AuthUserDetails;
 import io.mosip.preregistration.core.util.AuthTokenUtil;
 
 public class DataSyncRestInterceptor implements ClientHttpRequestInterceptor {
 
-	@Autowired
 	@Lazy
+	@Autowired
 	private AuthTokenUtil tokenUtil;
 
 	@Override
@@ -27,9 +29,24 @@ public class DataSyncRestInterceptor implements ClientHttpRequestInterceptor {
 
 	private void addHeadersToRequest(HttpRequest httpRequest, byte[] bytes) {
 		HttpHeaders headers = httpRequest.getHeaders();
-		String token = tokenUtil.getToken();
-		System.out.println(token);
-		headers.set(HttpHeaders.COOKIE, token);
+		if (httpRequest.getURI().toString().contains("preregistration")) {
+			AuthUserDetails authUserDetails = getAuthUserDetails();
+			if (authUserDetails != null)
+				headers.set(HttpHeaders.COOKIE, "Authorization=" + authUserDetails.getToken());
+		} else {
+			String token = tokenUtil.getToken();
+			headers.set(HttpHeaders.COOKIE, token);
+		}
+
+	}
+
+	private AuthUserDetails getAuthUserDetails() {
+		AuthUserDetails authUserDetails = null;
+		if (SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication() != null
+				&& SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof AuthUserDetails)
+
+			authUserDetails = (AuthUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		return authUserDetails;
 	}
 
 }
