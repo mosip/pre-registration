ca64e98e43e505aab787e4566b89ba696525e9da
Ajay1596 <44763643+Ajay1596@users.noreply.github.com>
2021-08-30 15:46:26 +0530

    updating contextpath

--- pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/controller/LoginController.java
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/controller/LoginController.java
@@ -171,7 +171,7 @@ public class LoginController {
 		responseCookie.setMaxAge((int) -1);
 		responseCookie.setHttpOnly(true);
 		responseCookie.setSecure(true);
-		responseCookie.setPath(contextPath);
+		responseCookie.setPath(contextPath + "/");
 		res.addCookie(responseCookie);
 
 		return ResponseEntity.status(HttpStatus.OK).body(loginService.validateWithUserIdOtp(userIdOtpRequest));
