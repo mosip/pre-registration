562bf54c8501f9b10a00afa5c8365a6cd2c73dfa
rambhatt1591 <77657115+rambhatt1591@users.noreply.github.com>
2021-09-15 11:48:11 +0530

    MOSIP-17445 Anonymous Profile Release Scripts (#302)

--- db_release_scripts/mosip_prereg/sql/1.2_prereg-scripts_release.sql
+++ db_release_scripts/mosip_prereg/sql/1.2_prereg-scripts_release.sql
@@ -7,11 +7,15 @@
 -- 
 -- Modified Date        Modified By         Comments / Remarks
 -- -------------------------------------------------------------------------------------------------
+-- Sept-2021		Ram Bhatt 	    Creation of Anonymous Profile Table
+----------------------------------------------------------------------------------------------------
 
 \c mosip_prereg sysadmin
 
 
 \ir ../ddl/prereg-applications.sql
+
+\ir  ../ddl/prereg-anonymous_profile.sql
 
 ALTER TABLE prereg.reg_appointment DROP CONSTRAINT IF EXISTS fk_rappmnt_id CASCADE;
 
