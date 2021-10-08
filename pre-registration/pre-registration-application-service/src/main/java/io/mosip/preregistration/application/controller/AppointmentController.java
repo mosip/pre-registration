d7394af9657808fc9bb75834c55b042e2f05a764
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2021-10-08 18:05:44 +0530

    Updated method name for internal cancel

--- pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/controller/AppointmentController.java
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/controller/AppointmentController.java
@@ -82,7 +82,7 @@ public class AppointmentController {
 	@PutMapping(path = "/applications/appointment/{preRegistrationId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiOperation(value = "Cancel an booked appointment")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Appointment cancelled successfully") })
-	public ResponseEntity<MainResponseDTO<CancelBookingResponseDTO>> internalCancelBook(
+	public ResponseEntity<MainResponseDTO<CancelBookingResponseDTO>> cancelBook(
 			@PathVariable("preRegistrationId") String preRegistrationId) {
 		log.info("Cancel the appointment for preRegId :{} ", preRegistrationId);
 		return ResponseEntity.status(HttpStatus.OK).body(appointmentService.cancelAppointment(preRegistrationId));
@@ -92,7 +92,7 @@ public class AppointmentController {
 	@PutMapping(path = "/internal/applications/appointment/{preRegistrationId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiOperation(value = "Cancel an booked appointment. Used internally by batch job,")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Appointment cancelled successfully") })
-	public ResponseEntity<MainResponseDTO<CancelBookingResponseDTO>> cancelBook(
+	public ResponseEntity<MainResponseDTO<CancelBookingResponseDTO>> internalCancelBook(
 			@PathVariable("preRegistrationId") String preRegistrationId) {
 		log.info("Cancel the appointment for preRegId called internally :{} ", preRegistrationId);
 		return ResponseEntity.status(HttpStatus.OK).body(appointmentService.cancelAppointment(preRegistrationId));
