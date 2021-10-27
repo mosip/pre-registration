826c2d3633a5a190181f6f07fb1e52197e93d395
kameshsr <kameshsr1338@gmail.com>
2021-10-27 14:35:07 +0530

    MOSIP-17753 Fixed https issue in captcha controller.

--- pre-registration/pre-registration-captcha-service/src/main/java/io/mosip/preregistration/captcha/config/SwaggerConfig.java
+++ pre-registration/pre-registration-captcha-service/src/main/java/io/mosip/preregistration/captcha/config/SwaggerConfig.java
@@ -1,22 +1,23 @@
 package io.mosip.preregistration.captcha.config;
 
-import java.util.Collections;
-import java.util.Map;
-
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.Collections;
+import java.util.Map;
 
 @Configuration
-@EnableSwagger2
 @ConfigurationProperties("mosip.preregistration.captcha")
 public class SwaggerConfig {
 
@@ -51,24 +52,40 @@ public class SwaggerConfig {
 	 */
 	private static final String DESCRIPTION = " Captcha Service for Generating and validating captcha";
 
-	/**
-	 * Produces {@link ApiInfo}
-	 * 
-	 * @return {@link ApiInfo}
-	 */
-	@Autowired
-	BuildProperties buildProperties;
-
-	@Bean
-	public OpenAPI customOpenAPI() {
-
-		return new OpenAPI()
-				.info(new Info().title(TITLE).version(buildProperties.getVersion()).description(DESCRIPTION));
-
-	}
-
 	@Bean
 	public RestTemplate restTemplateBean() {
 		return new RestTemplate();
 	}
+
+	private static final Logger logger = LoggerFactory.getLogger(SwaggerConfig.class);
+
+	@Autowired
+	private OpenApiProperties openApiProperties;
+
+	@Bean
+	public OpenAPI openApi() {
+		OpenAPI api = new OpenAPI()
+				.components(new Components())
+				.info(new Info()
+						.title(openApiProperties.getInfo().getTitle())
+						.version(openApiProperties.getInfo().getVersion())
+						.description(openApiProperties.getInfo().getDescription())
+						.license(new License()
+								.name(openApiProperties.getInfo().getLicense().getName())
+								.url(openApiProperties.getInfo().getLicense().getUrl())));
+
+		openApiProperties.getService().getServers().forEach(server -> {
+			api.addServersItem(new Server().description(server.getDescription()).url(server.getUrl()));
+		});
+		logger.info("swagger open api bean is ready");
+		return api;
+	}
+
+	@Bean
+	public GroupedOpenApi groupedOpenApi() {
+		return GroupedOpenApi.builder().group(openApiProperties.getGroup().getName())
+				.pathsToMatch(openApiProperties.getGroup().getPaths().stream().toArray(String[]::new))
+				.build();
+	}
+
 }
