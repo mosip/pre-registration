175efdfe744d71fe67eb5bfa29d2a03041e3f10e
Mayura Deshmukh <52281514+mayuradesh@users.noreply.github.com>
2021-11-09 12:51:55 +0530

    Revert "[MOSIP-18156] Fixed New Resttemplate issue"

--- pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/security/AuthenticationProvider.java
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/security/AuthenticationProvider.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -40,9 +39,7 @@ public class AuthenticationProvider extends AbstractUserDetailsAuthenticationPro
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationProvider.class);
 
-	@Autowired
-	@Qualifier(value = "restTemplateConfig")
-	private RestTemplate restTemplate;
+	private RestTemplate restTemplate = new RestTemplate();
 
 	@Value("${prereg.auth.jwt.secret}")
 	private String jwtSecret;
