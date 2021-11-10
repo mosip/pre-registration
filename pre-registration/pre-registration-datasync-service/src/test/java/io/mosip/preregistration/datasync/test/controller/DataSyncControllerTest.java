4b4388a8f6b47a6cfdbabd80e85ac59b9f82738e
Mahammed Taheer <mohd.taheer@gmail.com>
2021-11-10 20:57:47 +0530

    Fixed Failed Test cases.

--- pre-registration/pre-registration-datasync-service/src/test/java/io/mosip/preregistration/datasync/test/controller/DataSyncControllerTest.java
+++ pre-registration/pre-registration-datasync-service/src/test/java/io/mosip/preregistration/datasync/test/controller/DataSyncControllerTest.java
@@ -169,7 +169,7 @@ public class DataSyncControllerTest {
 		mainDataSyncResponseDTO.setResponse(preRegistrationIdsDTO);
 
 		Mockito.when(dataSyncService.retrieveAllPreRegIds(mainDataSyncRequestDTO)).thenReturn(mainDataSyncResponseDTO);
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/sync")
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/")
 				.contentType(MediaType.APPLICATION_JSON_VALUE).characterEncoding("UTF-8")
 				.accept(MediaType.APPLICATION_JSON_VALUE).content(jsonObject.toString());
 		mockMvc.perform(requestBuilder).andExpect(status().isOk());
@@ -192,7 +192,7 @@ public class DataSyncControllerTest {
 		mainReverseDataSyncResponseDTO.setResponse(reverseDatasyncReponseDTO);
 		Mockito.when(dataSyncService.storeConsumedPreRegistrations(mainReverseDataSyncRequestDTO))
 				.thenReturn(mainReverseDataSyncResponseDTO);
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/sync/consumedPreRegIds")
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/consumedPreRegIds")
 				.contentType(MediaType.APPLICATION_JSON_VALUE).characterEncoding("UTF-8")
 				.accept(MediaType.APPLICATION_JSON_VALUE).content(jsonObjectRev.toString());
 		System.out.println(requestBuilder);
