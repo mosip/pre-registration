76178a773d1fb6c5d3d4077ea8c21b15130b04e2
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2022-01-04 18:10:45 +0530

    added missing unit tests

---
+++ pre-registration/pre-registration-application-service/src/test/java/io/mosip/preregistration/application/test/controller/ProxyMasterdataControllerTest.java
@@ -0,0 +1,65 @@
+package io.mosip.preregistration.application.test.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import io.mosip.preregistration.application.controller.ProxyMasterdataController;
+import io.mosip.preregistration.application.service.ProxyMasterDataService;
+import io.mosip.preregistration.application.util.ProxyMasterdataServiceUtil;
+import net.minidev.json.parser.ParseException;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProxyMasterdataController.class)
+@Import(ProxyMasterdataController.class)
+@WithMockUser(username = "individual", authorities = { "INDIVIDUAL", "REGISTRATION_OFFICER" })
+public class ProxyMasterdataControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@MockBean
+	private ProxyMasterDataService proxyMasterDataService;
+
+	@MockBean
+	private ProxyMasterdataServiceUtil util;
+
+	@Before
+	public void setup() throws URISyntaxException, FileNotFoundException, ParseException {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void masterDataProxyControllerTest() throws Exception {
+		ResponseEntity<Object> response = new ResponseEntity<Object>(HttpStatus.OK);
+		Mockito.when(proxyMasterDataService.getMasterDataResponse(Mockito.any(), Mockito.any())).thenReturn(response);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/proxy/test")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.accept(MediaType.APPLICATION_JSON_VALUE);
+		mockMvc.perform(requestBuilder).andExpect(status().isOk());
+	}
+
+}
