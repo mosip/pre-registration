59026ae8f27dd00a938543fb2cc10c96aa1effa7
Aiham Hasan <Aiham.Hasan@mindtree.com>
2022-03-31 15:09:24 +0530

    Fixed Test case
    Signed-off-by: Aiham <aiham.hasan@mindtree.com>

--- pre-registration/pre-registration-captcha-service/src/test/java/io/mosip/preregistration/captcha/serviceimpl/test/CaptchaServiceImplTest.java
+++ pre-registration/pre-registration-captcha-service/src/test/java/io/mosip/preregistration/captcha/serviceimpl/test/CaptchaServiceImplTest.java
@@ -3,6 +3,7 @@ package io.mosip.preregistration.captcha.serviceimpl.test;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
