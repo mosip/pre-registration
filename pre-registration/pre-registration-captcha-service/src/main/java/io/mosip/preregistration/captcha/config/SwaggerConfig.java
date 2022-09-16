01aae7e93a47d6b4cebb2f81c2019d42868c24ee
urviljoshi <urvvil08@gmail.com>
2022-09-16 19:00:22 +0530

    bean defination modified

--- pre-registration/pre-registration-captcha-service/src/main/java/io/mosip/preregistration/captcha/config/SwaggerConfig.java
+++ pre-registration/pre-registration-captcha-service/src/main/java/io/mosip/preregistration/captcha/config/SwaggerConfig.java
@@ -5,21 +5,36 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.Map;
 
 @Configuration
 @ConfigurationProperties("mosip.preregistration.captcha")
 public class SwaggerConfig {
+	
+	
+	@Value("${preregistration.captchaservice.httpclient.connections.max.per.host:20}")
+	private int maxConnectionPerRoute;
+
+	@Value("${preregistration.captchaservice.httpclient.connections.max:100}")
+	private int totalMaxConnection;
 
 	/** The id. */
 	private Map<String, String> id;
@@ -53,8 +68,13 @@ public class SwaggerConfig {
 	private static final String DESCRIPTION = " Captcha Service for Generating and validating captcha";
 
 	@Bean
-	public RestTemplate restTemplateBean() {
-		return new RestTemplate();
+	public RestTemplate restTemplateBean() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+			HttpClientBuilder httpClientBuilder = HttpClients.custom()
+				.setMaxConnPerRoute(maxConnectionPerRoute)
+				.setMaxConnTotal(totalMaxConnection).disableCookieManagement();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setHttpClient(httpClientBuilder.build());
+		return new RestTemplate(requestFactory);
 	}
 
 	private static final Logger logger = LoggerFactory.getLogger(SwaggerConfig.class);
