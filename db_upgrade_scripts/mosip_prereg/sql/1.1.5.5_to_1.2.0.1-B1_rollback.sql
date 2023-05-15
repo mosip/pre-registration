6a5c3d7b83df486f3260b0422b0c4272aedf3a80
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-05-15 15:08:08 +0530

    Mosip 23833 (#595)
    * [MOSIP-23833] update db release scripts
    
    * [MOSIP-23833] added psql error handling
    
    * [MOSIP-24322] updated deploy.properties
    
    * [MOSIP-23833] restructered db upgrade scripts
    
    * Update upgrade.properties
    
    * Update upgrade.properties
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    
    * Update upgrade.properties
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>
    
    ---------
    
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>

---
+++ db_upgrade_scripts/mosip_prereg/sql/1.1.5.5_to_1.2.0.1-B1_rollback.sql
@@ -0,0 +1,8 @@
+\c mosip_prereg sysadmin
+
+DROP TABLE IF EXISTS prereg-applications;
+DROP TABLE IF EXISTS prereg-anonymous_profile;
+
+ALTER TABLE prereg.reg_appointment ADD CONSTRAINT fk_rappmnt_id FOREIGN KEY (prereg_id)
+REFERENCES prereg.applicant_demographic(prereg_id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
