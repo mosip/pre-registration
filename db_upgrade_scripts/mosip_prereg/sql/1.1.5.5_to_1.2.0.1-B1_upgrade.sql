36e99683d899c425ae512a3e0233064f4c1f8203
Aiham Hasan <Aiham.Hasan@mindtree.com>
2023-06-15 15:19:08 +0530

    [MOSIP-27987] Updated prereg db_upgrade_scripts
    Signed-off-by: Aiham <aiham.hasan@mindtree.com>

--- db_upgrade_scripts/mosip_prereg/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
+++ db_upgrade_scripts/mosip_prereg/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
@@ -16,6 +16,15 @@ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA prereg TO postgres;
 
 ALTER TABLE prereg.reg_appointment DROP CONSTRAINT IF EXISTS fk_rappmnt_id CASCADE;
 
+CREATE INDEX IF NOT EXISTS idx_app_demo_cr_by ON prereg.applicant_demographic USING btree cr_by COLLATE pg_catalog."default" ASC NULLS LAST) TABLESPACE pg_default;
+CREATE INDEX IF NOT EXISTS idx_app_demo_prid ON prereg.applicant_demographic USING btree (prereg_id COLLATE pg_catalog."default" ASC NULLS LAST) TABLESPACE pg_default;
+
+DROP TABLE IF EXISTS prereg.prid_seq CASCADE;
+DROP TABLE IF EXISTS prereg.transaction_type CASCADE;
+DROP TABLE IF EXISTS prereg.language_transliteration CASCADE;
+DROP TABLE IF EXISTS prereg.prid_seed CASCADE;
+DROP TABLE IF EXISTS prereg.pre_registration_transaction CASCADE;
+
 INSERT INTO prereg.applications(application_id, booking_type, booking_status_code, regcntr_id, appointment_date, booking_date,
    slot_from_time, slot_to_time, contact_info, cr_by, cr_dtimes, upd_by, upd_dtimes, application_status_code)
    Select t1.prereg_id, 'NEW_PREREGISTRATION', t1.status_code, t2.regcntr_id, t2.appointment_date,
