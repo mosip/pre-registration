83f222b313e167386fea0e79ea90bc91fcc1e30d
Keshav Mishra <chandrakeshavmishra@gmail.com>
2023-06-18 09:10:36 +0530

    [MOSIP-28035] Update 1.1.5.5_to_1.2.0.1-B1_rollback.sql
    Signed-off-by: Keshav Mishra <chandrakeshavmishra@gmail.com>

--- db_upgrade_scripts/mosip_prereg/sql/1.1.5.5_to_1.2.0.1-B1_rollback.sql
+++ db_upgrade_scripts/mosip_prereg/sql/1.1.5.5_to_1.2.0.1-B1_rollback.sql
@@ -1,7 +1,7 @@
 \c mosip_prereg
 
-DROP TABLE IF EXISTS prereg-applications;
-DROP TABLE IF EXISTS prereg-anonymous_profile;
+DROP TABLE IF EXISTS prereg.applications;
+DROP TABLE IF EXISTS prereg.anonymous_profile;
 
 ALTER TABLE prereg.reg_appointment ADD CONSTRAINT fk_rappmnt_id FOREIGN KEY (prereg_id)
 REFERENCES prereg.applicant_demographic(prereg_id) MATCH SIMPLE
