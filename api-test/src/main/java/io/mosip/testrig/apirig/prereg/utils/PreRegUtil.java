ee6d0143cebd95e4a45f3f635065b22b4dc9d167
Sradha Mohanty <mohantysradha10@gmail.com>
2025-07-30 17:40:53 +0530

    MOSIP-42259: Enable execution of a single test or task with all its required dependencies for PREREG. (#903)
    * MOSIP-40258
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOSIP-40258
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOSIP-28246 Removed unused variables from apitest-prereg module
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOSIP-37971 API is giving wrong error code in the response
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOSIP-37971 Added checkStatusCodeOnlyInResponse for blank PRID cases
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOSIP-37971 Removed tab
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOSIP-28246 Removed commented unused variables
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOSIP-42259 Enable execution of a single test or task with all its required dependencies clearly mapped and managed externally.
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOdified prereg properties file
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOSIP-42259 Changed the POM version
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    * MOSIP-42259
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    
    ---------
    
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/prereg/utils/PreRegUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/prereg/utils/PreRegUtil.java
@@ -1,5 +1,8 @@
 package io.mosip.testrig.apirig.prereg.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
@@ -14,6 +17,8 @@ public class PreRegUtil extends AdminTestUtil {
 
 	private static final Logger logger = Logger.getLogger(PreRegUtil.class);
 	
+	public static List<String> testCasesInRunScope = new ArrayList<>();
+	
 	public static void setLogLevel() {
 		if (PreRegConfigManager.IsDebugEnabled())
 			logger.setLevel(Level.ALL);
@@ -23,11 +28,17 @@ public class PreRegUtil extends AdminTestUtil {
 	
 	public static String isTestCaseValidForExecution(TestCaseDTO testCaseDTO) {
 		String testCaseName = testCaseDTO.getTestCaseName();
+		currentTestCaseName = testCaseName;
 		
 		int indexof = testCaseName.indexOf("_");
 		String modifiedTestCaseName = testCaseName.substring(indexof + 1);
 
 		addTestCaseDetailsToMap(modifiedTestCaseName, testCaseDTO.getUniqueIdentifier());
+		
+		if (!testCasesInRunScope.isEmpty()
+				&& testCasesInRunScope.contains(testCaseDTO.getUniqueIdentifier()) == false) {
+			throw new SkipException(GlobalConstants.NOT_IN_RUN_SCOPE_MESSAGE);
+		}
 
 		if (SkipTestCaseHandler.isTestCaseInSkippedList(testCaseName)) {
 			throw new SkipException(GlobalConstants.KNOWN_ISSUES);
