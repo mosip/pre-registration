6679f0955f33cd6254d40e0695fd157e7c6bf9c0
Nitin Hegde <165893206+hegdenitin@users.noreply.github.com>
2025-10-14 17:00:23 +0530

    MOSIP-43260 Develop code merge into release-1.3.x (#951)
    Signed-off-by: Nitin Hegde <nitin.k@cyberpwn.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/prereg/utils/PreRegUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/prereg/utils/PreRegUtil.java
@@ -14,26 +14,27 @@ import io.mosip.testrig.apirig.utils.GlobalConstants;
 import io.mosip.testrig.apirig.utils.SkipTestCaseHandler;
 
 public class PreRegUtil extends AdminTestUtil {
+
 	private static final Logger logger = Logger.getLogger(PreRegUtil.class);
-	
+
 	public static List<String> testCasesInRunScope = new ArrayList<>();
-	
+
 	public static void setLogLevel() {
 		if (PreRegConfigManager.IsDebugEnabled())
 			logger.setLevel(Level.ALL);
 		else
 			logger.setLevel(Level.ERROR);
 	}
-	
+
 	public static String isTestCaseValidForExecution(TestCaseDTO testCaseDTO) {
 		String testCaseName = testCaseDTO.getTestCaseName();
 		currentTestCaseName = testCaseName;
-		
+
 		int indexof = testCaseName.indexOf("_");
 		String modifiedTestCaseName = testCaseName.substring(indexof + 1);
 
 		addTestCaseDetailsToMap(modifiedTestCaseName, testCaseDTO.getUniqueIdentifier());
-		
+
 		if (!testCasesInRunScope.isEmpty()
 				&& testCasesInRunScope.contains(testCaseDTO.getUniqueIdentifier()) == false) {
 			throw new SkipException(GlobalConstants.NOT_IN_RUN_SCOPE_MESSAGE);
@@ -42,17 +43,17 @@ public class PreRegUtil extends AdminTestUtil {
 		if (SkipTestCaseHandler.isTestCaseInSkippedList(testCaseName)) {
 			throw new SkipException(GlobalConstants.KNOWN_ISSUES);
 		}
-		
+
 		JSONArray postalCodeArray = new JSONArray(getValueFromAuthActuator("json-property", "postal_code"));
 
 		if (testCaseName.startsWith("Prereg_")
 				&& (testCaseName.contains("_Invalid_PostalCode_")
-						|| testCaseName.contains("_SpacialCharacter_PostalCode_"))
+				|| testCaseName.contains("_SpacialCharacter_PostalCode_"))
 				&& (globalRequiredFields != null && !globalRequiredFields.toList().contains(postalCodeArray))) {
 			throw new SkipException(GlobalConstants.FEATURE_NOT_SUPPORTED_MESSAGE);
 		}
 
 		return testCaseName;
 	}
-	
+
 }
