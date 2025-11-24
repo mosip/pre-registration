352f840dd8554e17ddaad1e5cfa389e132bb3644
Nidhi <nidhi.k@cyberpwn.com>
2025-11-24 11:46:14 +0530

    Add comments for rollback script clarity
    Added comments to clarify the purpose of the rollback script.
    
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>

--- db_upgrade_scripts/mosip_prereg/sql/1.2.0.3_to_1.3.0_rollback.sql
+++ db_upgrade_scripts/mosip_prereg/sql/1.2.0.3_to_1.3.0_rollback.sql
@@ -1,4 +1,5 @@
 \c mosip_prereg
+-- Below script required to rollback from 1.3.0-beta.1 to 1.3.0.
 -- ------------------------------------------------------------------------------------------
 -- Rollback script for Migrating Spring batch version to 5.0 as part of Java 21 Migration.
 -- References:
