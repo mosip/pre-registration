402039ce64ddc0d83b0b5bec53f6ed242ff2e1b1
Nidhi <nidhi.k@cyberpwn.com>
2025-11-24 11:45:46 +0530

    -- Below script required to upgrade from 1.3.0-beta.1 to 1.3.0. added
    Signed-off-by: Nidhi <nidhi.k@cyberpwn.com>

--- db_upgrade_scripts/mosip_prereg/sql/1.2.0.3_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_prereg/sql/1.2.0.3_to_1.3.0_upgrade.sql
@@ -1,4 +1,5 @@
 \c mosip_prereg
+-- Below script required to upgrade from 1.3.0-beta.1 to 1.3.0.
 -- ------------------------------------------------------------------------------------------
 -- Upgrade script for Migrating Spring batch version to 5.0 as part of Java 21 Migration.
 -- References:
